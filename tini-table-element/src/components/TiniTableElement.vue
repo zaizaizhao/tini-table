42428193fc3ef201f7c1122824e3bb5f8f66f9eb
unknown <2534243725@qq.com>
2023-11-11 14:33:38 +0800

    单元格编辑优化，替换textarea

--- tini-table-element/src/components/TiniTableElement.vue
+++ tini-table-element/src/components/TiniTableElement.vue
@@ -1,25 +1,27 @@
 <script setup lang="ts">
-import { onBeforeUnmount, onMounted, reactive, ref } from 'vue';
+import { onBeforeUnmount, onMounted, reactive, ref } from "vue";
 
 const props = defineProps({
-    title: {
-        type: String,
-        default: "haha"
-    },
-    rows: {
-        type: Number,
-        default: 0
-    },
-    cols: {
-        type: Number,
-        default: 0
-    }
+  title: {
+    type: String,
+    default: "haha",
+  },
+  rows: {
+    type: Number,
+    default: 0,
+  },
+  cols: {
+    type: Number,
+    default: 0,
+  },
 });
 const isMouseDown = ref(false);
 const startCell = ref<HTMLTableCellElement | any>();
 const endCell = ref<HTMLTableCellElement | any>();
 const tiniTable = ref<HTMLTableElement | any>();
-const chosenCells = ref<HTMLTableCellElement[]>([])
+const chosenCells = ref<HTMLTableCellElement[]>([]);
+const editedCell = ref<CellType>();
+
 // const startRow = ref<number | undefined>();
 // const endRow = ref<number | undefined>();
 
@@ -27,136 +29,222 @@ const chosenCells = ref<HTMLTableCellElement[]>([])
 // const endCol = ref<number | undefined>();
 /**
  * 初始化表格参数
- * @param rows 
- * @param cols 
- * @param content 
+ * @param rows
+ * @param cols
+ * @param content
  */
 const tableInit = <T>(rows: number, cols: number, content: T): T[][] => {
-    const res = [];
-    for (let i = 0; i < rows; i++) {
-        const temp = [];
-        for (let j = 0; j < cols; j++) {
-            temp.push(content);
-        }
-        res.push([...temp])
+  const res = [];
+  for (let i = 0; i < rows; i++) {
+    const temp = [];
+    for (let j = 0; j < cols; j++) {
+      temp.push(JSON.parse(JSON.stringify(content)));
     }
-    return res;
-}
+    res.push([...temp]);
+  }
+  return res;
+};
 
-const tableInfo = reactive(tableInit(props.rows, props.cols, { value: '1', edit: false }));
+const tableInfo = reactive(
+  tableInit(props.rows, props.cols, { value: "1", edit: false })
+);
 
-function mousedown(event: MouseEvent): void {
-    startCell.value = event.target as HTMLTableCellElement;
-    endCell.value = event.target as HTMLTableCellElement;
-    cancelChosen()
-    chooseCell();
-    isMouseDown.value = true;
+function mousedown(event: MouseEvent, data: CellType): void {
+  if (editedCell.value && editedCell.value != data) {
+    editedCell.value.edit = false;
+  }
+  let node;
+  if((event.target as HTMLElement).tagName  != "TD"){
+   node = (event.target as HTMLElement).parentNode
+  }else{
+    node = event.target;
+  }
+  startCell.value =node as HTMLTableCellElement;
+  endCell.value = node as HTMLTableCellElement;
+  cancelChosen();
+  chooseCell();
+  isMouseDown.value = true;
 }
 
 function mouseover(event: MouseEvent) {
-    if (!isMouseDown.value) {
-        return;
-    }
-    endCell.value = event.target as HTMLTableCellElement;
-    chooseCell()
+  if (!isMouseDown.value) {
+    return;
+  }
+  let node;
+  if((event.target as HTMLElement).tagName  != "TD"){
+   node = (event.target as HTMLElement).parentNode
+  }else{
+    node = event.target;
+  }
+  endCell.value = node as HTMLTableCellElement;
+  chooseCell();
 }
 
 function mouseup(event: MouseEvent) {
-    if (isMouseDown.value) {
-        isMouseDown.value = false;
-    }
+  if (isMouseDown.value) {
+    isMouseDown.value = false;
+  }
 }
 
 function chooseCell(): void {
-    let [startRow, endRow] = [
-        (startCell.value?.parentNode as HTMLTableRowElement).rowIndex,
-        (endCell.value?.parentNode as HTMLTableRowElement).rowIndex
-    ].sort();
-    let [startCol, endCol] = [
-        startCell.value?.cellIndex,
-        endCell.value?.cellIndex
-    ].sort();
-    for (let i = 0; i < props.rows; i++) {
-        let rows = tiniTable.value?.rows[i]
-        for (let j = 0; j < props.cols; j++) {
-            let cell = rows.cells[j];
-            if (i >= startRow && i <= endRow && j >= startCol && j <= endCol) {
-                cell.style.backgroundColor = 'gray';
-            } else {
-                cell.style.backgroundColor = '';
-            }
-        }
+  let [startRow, endRow] = [
+    (startCell.value?.parentNode as HTMLTableRowElement).rowIndex,
+    (endCell.value?.parentNode as HTMLTableRowElement).rowIndex,
+  ].sort();
+  let [startCol, endCol] = [
+    startCell.value?.cellIndex,
+    endCell.value?.cellIndex,
+  ].sort();
+  for (let i = 0; i < props.rows; i++) {
+    let rows = tiniTable.value?.rows[i];
+    for (let j = 0; j < props.cols; j++) {
+      let cell = rows.cells[j];
+      if (i >= startRow && i <= endRow && j >= startCol && j <= endCol) {
+        cell.style.backgroundColor = "gray";
+      } else {
+        cell.style.backgroundColor = "";
+      }
     }
+  }
 }
 
 function cancelChosen() {
-    const cells = Array.prototype.slice.call(tiniTable.value?.getElementsByTagName('td'));
-    chosenCells.value.forEach(cell => {
-        cell.style.backgroundColor = 'white'
-    })
-    chosenCells.value = []
+  const cells = Array.prototype.slice.call(
+    tiniTable.value?.getElementsByTagName("td")
+  );
+  chosenCells.value.forEach((cell) => {
+    cell.style.backgroundColor = "white";
+  });
+  chosenCells.value = [];
+}
+
+//! 开始编辑单元格
+function startEdit(value: CellType) {
+  console.log(value);
+if(value.edit){
+  return
+}
+  editedCell.value = value;
+  value.edit = true;
+  setTimeout(() => {
+    const targetEle = document.querySelector(".edit-textarea") as HTMLElement;
+    targetEle.innerText = value.value;
+    set_focus(targetEle);
+    targetEle.addEventListener("blur", () => {
+      console.log("blur",editedCell.value);
+      editedCell.value!.value = targetEle.innerText;
+      value.value= targetEle.innerText
+    });
+  }, 10);
+}
+
+function set_focus(el: HTMLElement) {
+  console.log(el);
+  el.contentEditable = 'true'
+  //	el.focus();
+  //创建一个range范围对象
+  const range = document.createRange();
+  //用于设置 Range，使其包含一个 Node的内容。
+  range.selectNodeContents(el);
+  //将包含着的这段内容的光标设置到最后去，true 折叠到 Range 的 start 节点，false 折叠到 end 节点。如果省略，则默认为 false .
+  range.collapse(false);
+  const sel = window.getSelection();
+  sel!.removeAllRanges();
+  sel!.addRange(range);
 }
 
 onMounted(() => {
-    window.addEventListener('click', cancelChosen)
-})
+  window.addEventListener("click", cancelChosen);
+});
 onBeforeUnmount(() => {
-    window.removeEventListener('click', cancelChosen)
-})
+  window.removeEventListener("click", cancelChosen);
+});
 
+export type CellType = {
+  value: any;
+  edit: boolean;
+};
 </script>
 
 <template>
-    <span>表格的行数：{{ props.rows }}</span>
-    <span>表格的列数：{{ props.cols }}</span>
-    <div class="menu">
-        <span class="menu-item">合并单元格</span>
-        <span class="menu-item">新增一行</span>
-        <span class="menu-item">新增一列</span>
-        <span class="menu-item">删除行</span>
-        <span class="menu-item">删除列</span>
-    </div>
-    <div>
-
-        <table @click.stop ref="tiniTable">
-            <tr v-for="row in  tableInfo  ">
-                <td @mousedown="mousedown($event)" @mouseover="mouseover($event)" @mouseup="mouseup($event)"
-                    v-for="data in  row ">
-                    <span v-if="data.edit">{{ data.value }}</span>
-                    <textarea v-if="!data.edit"></textarea>
-                </td>
-            </tr>
-        </table>
-    </div>
+  <span>表格的行数：{{ props.rows }}</span>
+  <span>表格的列数：{{ props.cols }}</span>
+  <div class="menu">
+    <span class="menu-item">合并单元格</span>
+    <span class="menu-item">新增一行</span>
+    <span class="menu-item">新增一列</span>
+    <span class="menu-item">删除行</span>
+    <span class="menu-item">删除列</span>
+  </div>
+  <div>
+    <table @click.stop ref="tiniTable">
+      <tr v-for="row in tableInfo">
+        <td
+          @mousedown.stop="mousedown($event, data)"
+          @mouseover.stop="mouseover($event)"
+          @mouseup.stop="mouseup($event)"
+          @dblclick="startEdit(data)"
+          v-for="(data, index) in row"
+          :index="index"
+        >
+          <div class="blur-textarea" v-if="!data.edit">
+            {{ data.value }}
+          </div>
+         
+          <div class="edit-textarea" v-if="data.edit" contenteditable="true">
+            {{ data.value }}
+          </div>
+        </td>
+      </tr>
+    </table>
+  </div>
 </template>
 
 <style scoped>
 table {
-    border-collapse: collapse;
-    /* 合并单元格边框 */
-    width: 100%;
-    border: 1px solid black
+  border-collapse: collapse;
+  /* 合并单元格边框 */
+  width: 500px !important;
+  border: 1px solid red;
 }
 
 td {
-    text-align: left;
-    padding: 8px;
-    user-select: none;
-    border: 1px solid black;
-    /* 添加底部横线 */
+  text-align: left;
+  height: 40px;
+  user-select: none;
+  border: 1px solid black;
+  max-width: 100px;
+  min-width: 100px;
+  /* 添加底部横线 */
 }
 
 .menu {
-    height: 25px;
-    width: 400px;
-    display: flex;
+  height: 25px;
+  width: 400px;
+  display: flex;
 
-    border: 1px solid black;
-    border-radius: 4%;
+  border: 1px solid black;
+  border-radius: 4%;
 }
 
 .menu-item {
-    flex: 1;
-    border: 1px solid black;
+  flex: 1;
+  border: 1px solid black;
+}
+.edit-textarea {
+  resize: none;
+  min-height: 40px;
+  max-height: 300px;
+  word-wrap: break-word;
+  /* overflow-x: hidden; */
+  outline: 0;
+}
+.blur-textarea {
+  resize: none;
+  min-height: 40px;
+  max-height: 300px;
+  word-wrap: break-word;
+  /* overflow-x: hidden; */
+  outline: 0;
 }
 </style>
